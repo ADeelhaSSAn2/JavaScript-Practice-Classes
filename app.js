732a86a679e45f47f2f69052e55826e7f619d1bd
Adeel Hassan <adeelhassan35033@gmail.com>
2024-05-24 11:49:00 +0500

    updated

---
+++ app.js
@@ -0,0 +1,42 @@
+let arr = ["adeel" , "fahad" , "hassan"];
+// forEach kuch return nhi krta
+
+let reternValue = arr.forEach(function(v , i){
+    console.log(v,i);
+})
+console.log(reternValue)
+
+
+arr.map(function(v , i){
+    console.log(v,i);
+})
+
+// map return krta hai . ak new array bana kr return krta hai,
+//  map iteration bhi kr rha hai 
+
+let result = arr.map(function(v,i){
+    return `hello, ${v}`
+ })
+  console.log(result)
+
+// kuch return nhi krwayegy to undefined return ajaega
+
+let sult = arr.map(function(v,i){
+
+})
+  console.log(sult)
+
+// array . filter bhi same iteration krta hai ye bhi value or index deta hai
+
+  let array = ["adeel" , "fahad" , "hassan"];
+  array.filter(function(v,i){
+console.log(v.i)
+return true
+  })
+
+  let abc = [{name: "adeel"},{name: "fahad"},{name: "adeel"}];
+
+  let newAbc = abc.filter(function(v,i){
+    return v.name === "adeel"
+  })
+console.log(newAbc)
